5c807be19a16cc1b54648f26e20d46ef55c0145d
Jonathan Enrique <jonathan.ariel.enrique@gmail.com>
2025-06-28 01:05:32 -0300

    primer commit

--- src/ar/edu/unlam/crafteando/CrafteandoTP.java
+++ src/ar/edu/unlam/crafteando/CrafteandoTP.java
@@ -3,6 +3,7 @@ import org.jpl7.*;
 
 public class CrafteandoTP {
     public static void main(String[] args) {
+    	
     	String ruta = "prolog/datos.pl";
     	Query cargar = new Query("consult", new Term[]{new Atom(ruta)});
     	// 1- Internamente se hace un -> consult('datos.pl'). => CARGA EL ARCHIVO
@@ -25,5 +26,6 @@ public class CrafteandoTP {
     	// Eliminar todos los es_padre
     	Query limpiar = new Query("retractall(es_padre(_, _))");
     	limpiar.hasSolution();
+    	
     }
 }
